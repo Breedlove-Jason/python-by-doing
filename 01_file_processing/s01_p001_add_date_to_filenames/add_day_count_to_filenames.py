6d8b677f2e65b4c19d2173a12c0468449537ce25
Jason Breedlove <jbreedlove@jasonbreedlove.dev>
2026-06-07 18:44:36 -0700

    Add functionality to append word count and day to filenames

--- 01_file_processing/s01_p001_add_date_to_filenames/add_day_count_to_filenames.py
+++ 01_file_processing/s01_p001_add_date_to_filenames/add_day_count_to_filenames.py
@@ -2,26 +2,36 @@
 Project: Add Date to Filenames
 Course: Learn Python by Doing with 100 Projects
 """
+
 from datetime import datetime
 import os
-
 
 
 def main() -> None:
     """Run the project."""
     print("Project starter: Add Date to Filenames")
-    directory = 'files'
-    filepaths  = os.listdir(directory)
-    print(filepaths)
-    # filenames = [for file in directory in range(filepaths)]
-    now = datetime.now()
-    day_name = now.strftime("%A")
-    print(day_name)
-    # loop over the files in the directory and add the date to the filename
-    for file in filepaths:
-        print(file)
 
-        # file.append(day_name)
-        # "file_{day_name}_{filepaths[file]}"
+    directory = "files"
+
+    filenames = os.listdir(directory)
+    print(filenames)
+
+    day = datetime.now().strftime("%A")
+
+    for filename in filenames:
+        filepath = os.path.join(directory, filename)
+
+        with open(filepath, "r") as file:
+            content = file.read()
+            words = content.split()
+            word_count = len(words)
+            new_filename = f"{filename[:-4]}-{word_count}-{day}.txt"
+
+            new_filepath = os.path.join(directory, new_filename)
+            os.rename(filepath, new_filepath)
+
+            print(f"Renamed {filename} to {new_filename}")
+
+
 if __name__ == "__main__":
     main()
